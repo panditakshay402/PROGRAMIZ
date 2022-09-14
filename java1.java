ac0652dd3070282aaa3f50f7340161e35dfd73be
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2022-09-14 23:34:20 +0530

    Add files via upload

---
+++ java1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class java1 //example 2 included
+{
+public static void main(String args[])
+{
+Scanner scan=new Scanner(System.in);
+System.out.println("Enter a num");
+
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=a+b;
+System.out.println("addition of "+a+" & "+b+" is "+c);
+
+}}
