0fa6c3d8c614b2f452b882e3ee868b569ebb8329
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2022-09-15 19:11:58 +0530

    Add files via upload

---
+++ java4.java
@@ -0,0 +1,12 @@
+import java.util.*;          //char ASCII code print.
+class java4{
+public static void main(String args[])
+{
+  Scanner scan=new Scanner(System.in);
+char a=scan.next().charAt(0);
+int b=a;
+System.out.println(b);
+
+
+
+}}
