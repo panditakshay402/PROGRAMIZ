5280d50908e71dfde69323dbda4b99f233855960
Akshay pandit <51216244+panditakshay402@users.noreply.github.com>
2022-09-15 18:37:14 +0530

    Add files via upload

---
+++ java3.java
@@ -0,0 +1,14 @@
+import java.util.*;        //two floating point num multiplied.
+class java3
+{ public static void main(String aargs[])
+{
+ Scanner scan=new Scanner(System.in);
+float num1=scan.nextFloat();
+float num2=scan.nextFloat();
+float num3=num1*num2;
+
+System.out.println("floating num multiply "+num1+" & "+num2+" is "+num3);
+
+
+}
+}
